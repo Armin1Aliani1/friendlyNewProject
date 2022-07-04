cd61369237f8bc7a4399139d1381ba2e7c25a3e2
Dragon2623 <armin1.aliani1@gmail.com>
2022-07-05 00:37:09 +0430

    add User and Costumer

---
+++ friendlyNewProject/src/main/java/newProject/domain/Costumer.java
@@ -0,0 +1,37 @@
+package newProject.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Costumer extends User {
+    private String address;
+    private int age;
+    private String phoneNumber;
+
+    public Costumer() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
