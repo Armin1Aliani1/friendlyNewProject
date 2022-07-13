c33371d7a010412bfecb5cfc18195bdf863d328a
Dragon2623 <armin1.aliani1@gmail.com>
2022-07-14 02:48:14 +0430

    edit ApplicationOnlineShop

--- friendlyNewProject/src/main/java/newProject/ApplicationOnlineShop.java
+++ friendlyNewProject/src/main/java/newProject/ApplicationOnlineShop.java
@@ -1,15 +1,24 @@
 package newProject;
 
+import newProject.util.ApplicationContext;
 import newProject.util.Menu;
 
 public class ApplicationOnlineShop {
 
     public static void main(String[] args) {
+
         Menu.printMainMenu();
         int selection = Menu.selectMainMenu();
 
         if (selection == 1) {
-            // Login admin
+            ApplicationContext.adminService.login();
+            Menu.printAdminMenu();
+            selection = Menu.selectAminMenu();
+            if (selection==1){
+                // Add product
+            }else {
+                // Delete product
+            }
         } else {
             // Login costumer
         }
